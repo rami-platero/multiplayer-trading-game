15ebc1cf742ca8611707f8c174eb568b719645fe
Inzanic <ramiplatero11@gmail.com>
2023-05-26 20:50:56 -0300

    fixing cors

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Response } from "express";
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
-import cors from 'cors'
-import morgan from 'morgan'
+import cors from "cors";
+import morgan from "morgan";
 import { userRouter } from "./routes/users";
 import http from "http";
 import { initSocket } from "./sockets/socket";
@@ -10,15 +10,18 @@ import { tradeRouter } from "./routes/trades";
 
 export const app = express();
 export const server = http.createServer(app);
-initSocket()
+initSocket();
 
 app.use(express.json());
-app.use(cors())
-app.use(morgan("dev"))
+app.use(cors({
+    origin: 'https://multiplayer-trading-game.vercel.app/',
+    credentials: true,
+  }));
+app.use(morgan("dev"));
 
-app.use("/", userRouter,tradeRouter);
-app.get("/", (_req,res)=>{
-    res.setHeader("Access-Control-Allow-Credentials","true")
-  })
+app.get("/", (_req, res:Response) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+});
+app.use("/", userRouter, tradeRouter);
 
 app.set("port", process.env.PORT);
