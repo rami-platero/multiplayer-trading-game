4ac5e4d99ef9198d33f6de33e61fe0f082c3b2ef
Rami Platero <ramiplatero11@gmail.com>
2023-08-31 23:11:57 -0300

    add ToastContainer

--- client/src/components/Auth/AuthScreen.tsx
+++ client/src/components/Auth/AuthScreen.tsx
@@ -1,7 +1,9 @@
-import { useState} from "react";
+import { useState } from "react";
 import "./auth.css";
 import Login from "./Login";
 import Register from "./Register";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 enum AuthState {
   Login = "Login",
@@ -11,29 +13,30 @@ enum AuthState {
 const AuthScreen = () => {
   const [authState, setAuthState] = useState<AuthState>(AuthState.Login);
   return (
-      <div className="auth-container">
-        <h1>Welcome!</h1>
-        <div className="auth-selector">
-          <h3
-            className={`title ${authState === "Register"}`}
-            onClick={() => {
-              setAuthState(AuthState.Register);
-            }}
-          >
-            Sign Up
-          </h3>
-          <h3
-            className={`title ${authState === "Login"}`}
-            onClick={() => {
-              setAuthState(AuthState.Login);
-            }}
-          >
-            Login
-          </h3>
-        </div>
-        {authState === "Login" && <Login />}
-        {authState === "Register" && <Register />}
+    <div className="auth-container">
+      <ToastContainer limit={1} />
+      <h1>Welcome!</h1>
+      <div className="auth-selector">
+        <h3
+          className={`title ${authState === "Register"}`}
+          onClick={() => {
+            setAuthState(AuthState.Register);
+          }}
+        >
+          Sign Up
+        </h3>
+        <h3
+          className={`title ${authState === "Login"}`}
+          onClick={() => {
+            setAuthState(AuthState.Login);
+          }}
+        >
+          Login
+        </h3>
       </div>
+      {authState === "Login" && <Login />}
+      {authState === "Register" && <Register />}
+    </div>
   );
 };
 
